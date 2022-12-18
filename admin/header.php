c05f343d68bae72723d3a3b0664b0946593d2641
RAVINDRA SINGH <102677356+ravindra1998@users.noreply.github.com>
2022-12-18 14:04:50 +0530

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- Designined by CodingLab | www.youtube.com/codinglabyt -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+    <a href="index.php">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">ESTRental</span></a>
+    </div>
+      <ul class="nav-links">
+        <li>
+          <a href="View Flat.php" class="active">
+            <i class='bx bx-grid-alt' ></i>
+            <span class="links_name">View Flat</span>
+          </a>
+        </li>
+        <li>
+          <a href="Add Flat.php">
+            <i class='bx bx-box' ></i>
+            <span class="links_name">Add Flat</span>
+          </a>
+        </li>
+        <li>
+          <a href="View Customers.php">
+            <i class='bx bx-list-ul' ></i>
+            <span class="links_name">View Customers</span>
+          </a>
+        </li>
+        <li>
+          <a href="View Properties.php">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="links_name">View Properties</span>
+          </a>
+        </li>
+        
+      </ul>
+  </div>
